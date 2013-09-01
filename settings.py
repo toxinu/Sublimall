9b81dc43481c7b4ff320a022e346c91d24662bbd
Florian PAQUET <fpaquet@live.fr>
2013-09-01 21:01:02 +0200

    Added temporary port

--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@
 from urllib.parse import urljoin
 
 
-API_ROOT_URL = 'http://sublimesync.florianpaquet.com/'
+API_ROOT_URL = 'http://sublimesync.florianpaquet.com:8080/'
 API_UPLOAD_URL = urljoin(API_ROOT_URL, '/upload/')
 API_RETRIEVE_URL = urljoin(API_ROOT_URL, '/retrieve/')
