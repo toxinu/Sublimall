fff68c2e0c10570e518f15ef00ff63a18df5cd5a
Florian PAQUET <fpaquet@live.fr>
2013-09-01 20:58:12 +0200

    Added root api url

--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@
 from urllib.parse import urljoin
 
 
-API_ROOT_URL = 'http://192.168.1.10:8000/'
+API_ROOT_URL = 'http://sublimesync.florianpaquet.com/'
 API_UPLOAD_URL = urljoin(API_ROOT_URL, '/upload/')
 API_RETRIEVE_URL = urljoin(API_ROOT_URL, '/retrieve/')
