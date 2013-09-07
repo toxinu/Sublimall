a97785c665b25e68df9995f0e65abe71c227b301
Florian PAQUET <fpaquet@live.fr>
2013-09-08 01:53:42 +0200

    Updated urls

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@
 from urllib.parse import urljoin
 
 
-API_ROOT_URL = 'http://sublimesync.florianpaquet.com:8080/'
-API_UPLOAD_URL = urljoin(API_ROOT_URL, '/upload/')
-API_RETRIEVE_URL = urljoin(API_ROOT_URL, '/retrieve/')
+API_ROOT_URL = 'http://www.sublimesync.org'
+API_UPLOAD_URL = urljoin(API_ROOT_URL, '/api/upload/')
+API_RETRIEVE_URL = urljoin(API_ROOT_URL, '/api/retrieve/')
 BACKUP_DIRECTORY_NAME = 'backups'
