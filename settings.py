d16836f5c14f9bcd7bee0adf151127c4ea5a9dfc
Geoffrey Lehée <hello@socketubs.org>
2014-02-07 22:21:54 +0100

    Change API_ROOT_URL

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@
 from urllib.parse import urljoin
 
 
-API_ROOT_URL = 'http://www.sublimesync.org'
+API_ROOT_URL = 'http://localhost:8000'
 API_UPLOAD_URL = urljoin(API_ROOT_URL, '/api/upload/')
 API_RETRIEVE_URL = urljoin(API_ROOT_URL, '/api/retrieve/')
 BACKUP_DIRECTORY_NAME = 'backups'
