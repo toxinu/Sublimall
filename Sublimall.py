2409cdf90fc8773bf71a88bd6d6b6d2dc9c69762
Geoffrey Lehée <hello@socketubs.org>
2014-02-15 12:09:48 +0100

    Added plugin_loaded method in Sublimall.py

--- Sublimall.py
+++ Sublimall.py
@@ -2,23 +2,22 @@
 import sys
 import sublime
 from imp import reload
-from .sublimall.utils import get_7za_bin
+
+if sys.version_info.major == 2:
+    msg = "Sublimall is only available for SublimeText 3.\n Sorry about that."
+    sublime.error_message(msg)
 
 reloader_name = 'Sublimall.sublimall.reloader'
 if reloader_name in sys.modules:
     reload(sys.modules[reloader_name])
 
-load_sublimall = True
-settings = sublime.load_settings('Sublimall.sublime-settings')
-if sys.version_info.major == 2:
-    msg = "Sublimall is only available for SublimeText 3.\n Sorry about that."
-    sublime.error_message(msg)
-    load_sublimall = False
-if get_7za_bin() is None:
-    msg = "Sublimall need 7zip to archive and encrypt your configuration.\nInstall it. "
-    sublime.error_message(msg)
-    load_sublimall = False
 
-if load_sublimall:
-    from .sublimall import reloader
-    from .sublimall.commands import *
+def plugin_loaded():
+    from .sublimall.utils import get_7za_bin
+
+    if get_7za_bin() is None:
+        msg = "Sublimall need 7zip to archive and encrypt your configuration.\nInstall it. "
+        sublime.error_message(msg)
+
+from .sublimall import reloader
+from .sublimall.commands import *
