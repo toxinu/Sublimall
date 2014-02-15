ddc004b6a436dc9b8e297a6565ec06c6fd795fbb
Geoffrey Lehée <hello@socketubs.org>
2014-02-15 12:28:28 +0100

    Update to 0.0.5

--- sublimall/__init__.py
+++ sublimall/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = "0.0.4"
-__version_info__ = (0, 0, 4)
+__version__ = "0.0.5"
+__version_info__ = (0, 0, 5)
 
 SETTINGS_USER_FILE = "Sublimall.sublime-settings"
