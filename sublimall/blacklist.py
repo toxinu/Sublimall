27cf8c4ae0b56722ad318d5f6237677b5942efef
Geoffrey Lehée <hello@socketubs.org>
2014-02-18 00:19:10 +0100

    Added package excluded in settings

--- sublimall/blacklist.py
+++ sublimall/blacklist.py
@@ -1,4 +1,6 @@
 # -*- coding:utf-8 -*-
+import sublime
+from . import SETTINGS_USER_FILE
 
 # Path: Packages/
 packages = [
@@ -7,3 +9,11 @@ packages = [
 # Path: Installed Packages/
 installed_packages = [
     'Sublimall.sublime-package', ]
+
+
+def get_ignored_packages():
+    __settings = sublime.load_settings(SETTINGS_USER_FILE)
+    custom_packages = []
+    for ignored_package in __settings.get('ignore_packages'):
+        custom_packages.append(ignored_package)
+    return custom_packages
