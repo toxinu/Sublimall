c8b550b28a8e91e195dcbc4579f223e549f75163
Geoffrey LEHEE <g.lehee@jurismarches.com>
2014-03-07 12:06:14 +0100

    release 0.0.10

--- sublimall/__init__.py
+++ sublimall/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = "0.0.5"
-__version_info__ = (0, 0, 5)
+__version__ = "0.0.10"
+__version_info__ = (0, 0, 10)
 
 SETTINGS_USER_FILE = "Sublimall.sublime-settings"
