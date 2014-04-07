99feafd39af9594aa12d26c0b8736006d5d3fa51
Geoffrey LEHEE <g.lehee@jurismarches.com>
2014-04-07 14:17:35 +0200

    Better os detection and 7zip detection

--- sublimall/utils.py
+++ sublimall/utils.py
@@ -1,10 +1,37 @@
 # -*- coding:utf-8 -*-
 import os
+import sys
 import uuid
 import shutil
 import sublime
 import tempfile
+import platform
 from . import SETTINGS_USER_FILE
+from .logger import logger
+
+
+def is_linux():
+    return platform.system().lower() == 'linux'
+
+
+def is_osx():
+    return platform.system().lower() == 'darwin'
+
+
+def is_win():
+    return ('win32' in str(sys.platform).lower())
+
+
+def humansize(nbytes):
+    suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+    if nbytes == 0:
+        return '0 B'
+    i = 0
+    while nbytes >= 1024 and i < len(suffixes) - 1:
+        nbytes /= 1024.
+        i += 1
+    f = ('%.2f' % nbytes).rstrip('0').rstrip('.')
+    return '%s %s' % (f, suffixes[i])
 
 
 def get_7za_bin():
@@ -13,16 +40,21 @@ def get_7za_bin():
 
     if settings.get('7za_path') and os.path.exists(settings.get('7za_path')):
         zip_bin = settings.get('7za_path')
-    elif shutil.which('7z'):
-        zip_bin = shutil.which('7z')
     elif shutil.which('7za'):
         zip_bin = shutil.which('7za')
-    elif os.name == 'nt':
+    elif shutil.which('7z'):
+        zip_bin = shutil.which('7z')
+    elif shutil.which('p7zip'):
+        zip_bin = shutil.which('p7zip')
+    elif is_win():
         if os.path.exists(os.path.join(os.environ.get('ProgramFiles'), '7-Zip', '7z.exe')):
             zip_bin = os.path.join(os.environ.get('ProgramFiles'), '7-Zip', '7z.exe')
         if os.path.exists(os.path.join(os.environ.get('ProgramFiles(x86)'), '7-Zip', '7z.exe')):
             zip_bin = os.path.join(os.environ.get('ProgramFiles(x86)'), '7-Zip', '7z.exe')
+    elif is_osx():
+        zip_bin = "/usr/local/Cellar/p7zip/*/bin/7za"
 
+    logger.info('7za_path detected: %s' % zip_bin)
     return zip_bin
 
 
