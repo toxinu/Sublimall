92db18a1eb2b081fc9f86412f5ac50ef7d8b4c4d
Geoffrey LEHEE <g.lehee@jurismarches.com>
2014-04-07 14:16:53 +0200

    Save previous message in CommandStatus

--- sublimall/commands/command.py
+++ sublimall/commands/command.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+import copy
 import sublime
 
 
@@ -6,12 +7,14 @@ class MessageStatus(object):
     def __init__(self, message=""):
         self.is_running = False
         self.message = message
+        self.previous_message = ""
 
     def set_message(self, message):
+        self.previous_message = copy.deepcopy(self.message)
         self.message = message
 
     def run(self):
-        sublime.status_message(self.message)
+        sublime.status_message("[Sublimall] %s" % self.message)
         if self.is_running:
             sublime.set_timeout(lambda: self.run(), 100)
 
@@ -25,15 +28,16 @@ class CommandWithStatus(object):
         self._messageStatus = MessageStatus()
         sublime.set_timeout(lambda: self.unset_message, 1000)
 
-    def set_timed_message(self, message, time=5, clear=False):
+    def set_timed_message(self, message, time=7, clear=False):
         """
         Show a message for a specified amount of time
         and re-set the previous after it.
         """
-        old_message = self._messageStatus.message
         self.set_message(message)
         if not clear:
-            sublime.set_timeout(lambda: self.set_message(old_message), time * 1000)
+            sublime.set_timeout(
+                lambda: self.set_message(self._messageStatus.previous_message),
+                time * 1000)
         else:
             self.unset_message()
 
