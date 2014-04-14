615e485dd04a8594ccc47375c2c115447d9436f3
Geoffrey Lehée <hello@socketubs.org>
2014-04-14 23:25:06 +0200

    0.0.12

--- sublimall/__init__.py
+++ sublimall/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version_info__ = (0, 0, 11)
+__version_info__ = (0, 0, 12)
 __version__ = '.'.join(str(x) for x in __version_info__)
 
 SETTINGS_USER_FILE = "Sublimall.sublime-settings"
