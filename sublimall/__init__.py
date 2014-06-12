f6a3d2ffde01ddf9dc5bb58f4bb041ae549547a6
Geoffrey Lehée <geoffrey.lehee@lengow.com>
2014-06-12 14:31:39 +0200

    0.0.14

--- sublimall/__init__.py
+++ sublimall/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version_info__ = (0, 0, 13)
+__version_info__ = (0, 0, 14)
 __version__ = '.'.join(str(x) for x in __version_info__)
 
 SETTINGS_USER_FILE = "Sublimall.sublime-settings"
